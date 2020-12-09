be27e7a367febc0ef00b9a3c1d6cf2dde68ab6bd
Aparajita Fishman <aparajita@aparajita.com>
2020-12-09 12:43:06 -0800

    chore: change babel target

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: [['@vue/cli-plugin-babel/preset', { targets: { chrome: '57' }, useBuiltIns: 'entry' }]]
+  presets: [
+    [
+      '@vue/cli-plugin-babel/preset',
+      { targets: { chrome: '50' }, useBuiltIns: 'entry' }
+    ]
+  ]
 }
