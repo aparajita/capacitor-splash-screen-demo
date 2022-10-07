a2033cc372e45d320d04e6752f4a1d8f7e6a94ee
Aparajita <aparajita@aparajita.com>
2022-10-07 12:58:31 -0700

    fix: wrongs paths

--- ios/App/public/assets/status-tap.539eb834.js
+++
@@ -1,3 +0,0 @@
-import{b as a,f as i,d,w as c,s as l}from"./index.7966e040.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>d(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
--- ios/App/public/assets/swipe-back.cd3a751d.js
+++
@@ -1,3 +0,0 @@
-import{i as f,c as D,a as M}from"./index.7966e040.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const k=(o,h,m,g,p)=>{const r=o.ownerDocument.defaultView,c=f(o),X=t=>{const{startX:e}=t;return c?e>=r.innerWidth-50:e<=50},a=t=>c?-t.deltaX:t.deltaX,w=t=>c?-t.velocityX:t.velocityX;return D({el:o,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:t=>X(t)&&h(),onStart:m,onMove:t=>{const e=a(t)/r.innerWidth;g(e)},onEnd:t=>{const n=a(t),e=r.innerWidth,s=n/e,i=w(t),v=e/2,l=i>=0&&(i>.2||n>v),u=(l?1-s:s)*e;let d=0;if(u>5){const y=u/Math.abs(i);d=Math.min(y,540)}p(l,s<=0?.01:M(0,s,.9999),d)}})};export{k as createSwipeBackGesture};
